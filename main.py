2f5a88204cc04c73fc0ab8359947f3a60e60a146
Pavel <ratanov_pavel@mail.ru>
2026-08-15 17:49:48 +0500

    Auto-update: 15.08.2026 17:49:48,22

--- main.py
+++ main.py
@@ -103,7 +103,7 @@ def run_dual_ai_pipeline(prompt_style: str, task_id: str):
         num_inference_steps=4,
         strength=0.99,
         guidance_scale=0.0
-    ).images
+    ).images[0]
 
     final_filename = f"result_card_{task_id}.png"
     final_image.save(final_filename)
